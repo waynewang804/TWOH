fca837e01ff1d29385a0af7a2a88cb25a541b308
Wang <waynewn@amazon.com>
2026-04-10 17:41:56 +0800

    Fix GitHub Pages SPA 404 redirect and add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/TWOH">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
